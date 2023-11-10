809d63cbf03504f0968cea66f1e805b8b6b8bbe6
s102345 <Josephchen102345@gmail.com>
2023-11-10 21:17:10 +0800

    Update eval for TWCC

--- eval/run_evaluate.sh
+++ eval/run_evaluate.sh
@@ -1,4 +1,6 @@
-python -m eval.evaluate_origin \
+export PYTHONPATH="../..:$PYTHONPATH"
+
+python -m evaluate_origin \
 --coco_train_image_dir_path="data\train2014" \
 --coco_val_image_dir_path="data\val2014" \
 --coco_karpathy_json_path="data\karpathy_coco.json" \
