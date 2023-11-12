534b31b68f58810b70115012b401f96c5cecc768
Joseph Chen <Josephchen102345@gmail.com>
2023-11-12 18:00:05 +0800

    Update single process

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,11 +1,11 @@
 export PYTHONPATH="../..:$PYTHONPATH"
-export CUDA_VISIBLE_DEVICES="0,1"
+export CUDA_VISIBLE_DEVICES="0"
 
 python ../manager.py \
 --output_dir ./ \
 --detailed_log \
 --checkpoint_per_step \
---num_processes 2 \
+--num_processes 1 \
 --shots 2 \
 --num_samples 200 \
 --steps 5 \
