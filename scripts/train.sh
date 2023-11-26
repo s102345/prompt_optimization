ccab17a203cb238c21cb5a4412091d7813c0f98c
Joseph Chen <Josephchen102345@gmail.com>
2023-11-26 18:52:17 +0800

    update script

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,13 +1,15 @@
 export PYTHONPATH="../..:$PYTHONPATH"
-export CUDA_VISIBLE_DEVICES="0, 1"
+export CUDA_VISIBLE_DEVICES="0, 1, 2, 3, 4, 5, 6, 7"
 
 python ../manager.py \
 --output_dir ./ \
 --detailed_log \
 --checkpoint_per_step \
---num_processes 2 \
+--num_processes 8 \
 --shots 2 \
 --num_samples 200 \
---steps 5 \
---initial_prompt "A Image of" \
---example_rule "rand" 
+--steps 150 \
+--initial_prompt "What does the image describe?" \
+--optimization_task_number 1 \
+--example_number 3 \
+--example_rule "rices" 
